0c267cb282e319e1fa58ea73cf2873af3f097abc
vagrant <vagrant@homestead>
2018-08-13 07:33:32 +0000

    User can upload avatar

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,26 +5,36 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
     
     public function show(User $user){
+         
         return view('users.show', compact('user'));
     }
     
     public function edit(User $user)
     {
-        
+        //dd(config('app.url'));
          return view('users.edit', compact('user'));
     }
     
     
     //更新用户资料
-    public function update(UserRequest $request, User $user)
-    {
-        $user->update($request->all());
-        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
-    }
-    
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
+     {
+       
+         $data = $request->all();
+         if ($request->avatar) {
+             $result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
+             if ($result) {
+                 $data['avatar'] = $result['path'];
+             }
+         }
+
+         $user->update($data);
+         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
+     }
 }
--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'larabbs.test'),
+    'url' => env('http://larabbs.test', 'http://larabbs.test'),
 
     /*
     |--------------------------------------------------------------------------
